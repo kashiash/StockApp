20d5031e72cbc62882f5b45e04a3f97cb48af1c0
Jacek Kosinski <kashiash@gmail.com>
2023-12-31 22:49:23 +0100

    Initial Commit

---
+++ StockApp/StockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppApp.swift
+//  StockApp
+//
+//  Created by Jacek Kosinski U on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct StockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
