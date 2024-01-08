2f36ba84821c9a28b57aab81a616518f3776c2e2
Jacek Kosinski <kashiash@gmail.com>
2024-01-08 22:09:37 +0100

    uwierzytelnianie i sciezki

--- StockApp/StockAppApp.swift
+++ StockApp/StockAppApp.swift
@@ -15,10 +15,18 @@ struct StockAppApp: App {
 
 
     var body: some Scene {
+
+        let defaults = UserDefaults.standard
+        let token = defaults.string(forKey: "authToken")
+
         WindowGroup {
             NavigationStack(path: $appState.routes){
                 Group{
-                    RegistrationScreen()
+                    if token == nil {
+                        LoginScreen()
+                    } else {
+                        StockCategoryListScreen()
+                    }
                 }
                     .navigationDestination(for: Route.self) { route in
                         switch route {
